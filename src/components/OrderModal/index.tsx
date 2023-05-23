2443063497b55abc274a71b6269a249b1dac6fc1
Anderson <andersonsilvapp@gmail.com>
2023-05-23 00:36:01 -0300

    feat: request orders from api

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -10,9 +10,11 @@ interface OrderModalProps {
   visible: boolean;
   order: Order | null;
   onClose: () => void;
+  onCancelOrder: () => Promise<void>;
+  isLoading: boolean;
 }
 
-export function OrderModal({ visible, order, onClose }: OrderModalProps) {
+export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading }: OrderModalProps) {
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
       if (event.key === 'Escape') {
@@ -97,11 +99,20 @@ export function OrderModal({ visible, order, onClose }: OrderModalProps) {
         </OrderDetails>
 
         <Actions>
-          <button type="button" className='primary'>
+          <button
+            type="button"
+            className='primary'
+            disabled={isLoading}
+          >
             <span>⌛</span>
             <strong>Iniciar Produção</strong>
           </button>
-          <button type="button" className='secondary'>
+          <button
+            type="button"
+            className='secondary'
+            onClick={onCancelOrder}
+            disabled={isLoading}
+          >
             <strong>Cancelar pedido</strong>
           </button>
         </Actions>
---
+++ src/utils/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'http://172.19.135.138:3001'
+});
