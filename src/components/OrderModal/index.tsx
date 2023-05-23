e824fe88c4d2462dc2210046ade4d4e8af6f7186
Anderson <andersonsilvapp@gmail.com>
2023-05-23 01:02:52 -0300

    feat: change order status

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -12,9 +12,17 @@ interface OrderModalProps {
   onClose: () => void;
   onCancelOrder: () => Promise<void>;
   isLoading: boolean;
+  onChangeOrderStatus: () => void;
 }
 
-export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading }: OrderModalProps) {
+export function OrderModal({
+  visible,
+  order,
+  onClose,
+  onCancelOrder,
+  isLoading,
+  onChangeOrderStatus
+}: OrderModalProps) {
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
       if (event.key === 'Escape') {
@@ -99,14 +107,24 @@ export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading }
         </OrderDetails>
 
         <Actions>
-          <button
-            type="button"
-            className='primary'
-            disabled={isLoading}
-          >
-            <span>⌛</span>
-            <strong>Iniciar Produção</strong>
-          </button>
+          {order.status !== 'DONE' && (
+            <button
+              type="button"
+              className='primary'
+              disabled={isLoading}
+              onClick={onChangeOrderStatus}
+            >
+              <span>
+                {order.status === 'WAITING' && '🧑‍🍳'}
+                {order.status === 'IN_PRODUCTION' && '✅'}
+              </span>
+              <strong>
+                {order.status === 'WAITING' && 'Iniciar Produção'}
+                {order.status === 'IN_PRODUCTION' && 'Concluir Pedido'}
+              </strong>
+            </button>
+          )}
+
           <button
             type="button"
             className='secondary'
--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
+
 import { Order } from '../../types/Order';
 import { OrderModal } from '../OrderModal';
 import { Board, OrdersContainer } from './styles';
@@ -9,9 +11,11 @@ interface OrdersBoardProps {
   icon: string;
   orders: Order[];
   onCancelOrder: (orderId: string) => void;
+  onChangeOrderStatus: (orderId: string, status: Order['status']) => void;
+
 }
 
-export function OrdersBoard({ title, icon, orders, onCancelOrder }: OrdersBoardProps) {
+export function OrdersBoard({ title, icon, orders, onCancelOrder, onChangeOrderStatus }: OrdersBoardProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -26,11 +30,26 @@ export function OrdersBoard({ title, icon, orders, onCancelOrder }: OrdersBoardP
     setSelectedOrder(null);
   }
 
+  async function handleChangeOrderStatus() {
+    setIsLoading(true);
+
+    const status = selectedOrder?.status === 'WAITING'
+      ? 'IN_PRODUCTION'
+      : 'DONE';
+
+    await api.patch(`/orders/${selectedOrder?._id}`, { status });
+
+    toast.success(` O pedido da mesa ${selectedOrder?.table} teve o status alterado!`);
+    onChangeOrderStatus(selectedOrder!._id, status);
+    setIsLoading(false);
+    setIsModalVisible(false);
+  }
+
   async function handleCancelOrder() {
     setIsLoading(true);
 
-    await new Promise(resolve => setTimeout(resolve, 2000));
     await api.delete(`/orders/${selectedOrder?._id}`);
+    toast.success(` O pedido da mesa ${selectedOrder?.table} foi cancelado!`);
 
     onCancelOrder(selectedOrder!._id);
     setIsLoading(false);
@@ -45,11 +64,12 @@ export function OrdersBoard({ title, icon, orders, onCancelOrder }: OrdersBoardP
         onClose={handleCloseModal}
         onCancelOrder={handleCancelOrder}
         isLoading={isLoading}
+        onChangeOrderStatus={handleChangeOrderStatus}
       />
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>{orders.length}</span>
+        <span>({orders.length})</span>
       </header>
 
       <OrdersContainer >
