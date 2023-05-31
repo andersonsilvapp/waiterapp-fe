f0a7e37524ba91c9168496b283fe00493588b855
Anderson <andersonsilvapp@gmail.com>
2023-05-30 22:46:27 -0300

    feat: new routes structure: tableOrders

---
+++ src/pages/TableOrders/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header';
+import { Orders } from '../../components/Orders';
+
+export function TableOrders() {
+  return (
+    <>
+      <Header />
+      <Orders />
+    </>
+  );
+}
