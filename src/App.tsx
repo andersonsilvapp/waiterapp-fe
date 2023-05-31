209906ba44caf2eac76bd40736f644d14ea087ee
Anderson <andersonsilvapp@gmail.com>
2023-05-30 22:44:52 -0300

    feat: theme provider styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from 'styled-components';
+
 import 'react-toastify/dist/ReactToastify.css';
 
-import { Header } from './components/Header';
-import { Orders } from './components/Orders';
 import { GlobalStyles } from './styles/GlobalStyles';
+import { Routes } from './routes';
+import { theme } from './styles/theme';
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Header />
-      <Orders />
+      <Routes />
       <ToastContainer position='bottom-center' />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: '#3671E5',
+    secondary: '#00FF00',
+    white: '#fff',
+    grey: {
+      900: '#222222',
+      500: '#ababab'
+    },
+    green: '#2EA875',
+    red: '#FF4752'
+  },
+};
