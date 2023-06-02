6c12a48418930d88e13c1bb1044096af915a7bf4
Anderson <andersonsilvapp@gmail.com>
2023-06-02 15:02:50 -0300

    chore: add menu icons

---
+++ src/assets/images/icons/index.ts
@@ -0,0 +1,32 @@
+import customer from './customer.svg';
+import dashboard from './dashboard.svg';
+import home from './home.svg';
+import logo from './logo.svg';
+import message from './message.svg';
+import notification from './notification.svg';
+import order from './order.svg';
+import product from './product.svg';
+import setting from './setting.svg';
+
+export type IconsType =
+  | 'logo'
+  | 'home'
+  | 'dashboard'
+  | 'order'
+  | 'product'
+  | 'notification'
+  | 'customer'
+  | 'message'
+  | 'setting';
+
+export const icons = {
+  customer,
+  dashboard,
+  home,
+  logo,
+  message,
+  notification,
+  order,
+  product,
+  setting,
+};
